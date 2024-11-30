d901115ca75a5db3bcc773c3409a6160accfc2d8
walidrifaii <walidrifaii53@gmail.com>
2024-11-30 18:24:29 +0200

    fix

---
+++ README.md
@@ -0,0 +1,15 @@
+# Portfolio of Hachem Rifaii
+<img src ="./src/assets/Screenshot 2024-11-30 182318.png" />
+ 
+This project was created with CRA (Create React App)
+
+This is my portfolio website to introduce myself, here I put my skills, projects, and contact details.
+
+## Technologies used:
+- React
+- Typescript
+- Styled Components
+ 
+## To run this project:
+- yarn install
+- yarn run start
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,22 +1,31 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
+import phoneIcon from "../../assets/phone-icon.svg";
 import { Form } from "../Form/Form";
 
-
-export function Contact(){
-
-  return(
+export function Contact() {
+  return (
     <Container id="contact">
       <header>
         <h2>Contact</h2>
         <p>Ready to get started on your project? </p>
         <p>Contact me now for a Free consultation.</p>
       </header>
-    
-      <Form>
-   
-      </Form>
+      <div className="contacts">
+        <div>
+          <a href="mailto:walidrifaii53@gmail.com">
+            <img src={emailIcon} alt="Email" />
+          </a>
+          <a href="mailto:walidrifaii53@gmail.com">walidrifaii53@gmail.com</a>
+        </div>
+        <div>
+          <a href="tel:+96170657961">
+            <img src={phoneIcon} alt="Phone No" />
+          </a>
+          <a href="tel:+96170657961">(+961) 70657961</a>
+        </div>
+      </div>
+      <Form></Form>
     </Container>
-  )
+  );
 }
